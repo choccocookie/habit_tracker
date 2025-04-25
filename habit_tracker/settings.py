e76d490784961e42801195fcd52e4e0a585fed3b
choccocookie <Karanovvlad@gmail.com>
2025-04-25 13:18:24 +0300

    Docker

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = "habit_tracker.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
